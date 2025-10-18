283167fe97b392d43e776953a0cf7e44840c0626
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2025-10-18 19:44:52 +0530

    .

--- .config/bash/functions.bash
+++ .config/bash/functions.bash
@@ -34,10 +34,10 @@ print_header() {
 	set -- $(stty size)
 	local rows=$1
 	local cols=$2
-	local term_clr="${B_BLUE}${TERM}${B_WHITE}"
+	local term_clr="${B_BLUE}${TERM}${B_RED}"
 	local cols_clr="${B_CYAN}${cols}${B_WHITE}"
 	local rows_clr="${B_CYAN}${rows}${B_RED}"
-	echo "${ENDC}${BOLD}${B_RED}[${term_clr} - ${cols_clr} x ${rows_clr}]${ENDC}"
+	echo "${ENDC}${BOLD}${B_RED}[${term_clr}] ${B_WHITE}-${B_RED} [${cols_clr} x ${rows_clr}]${ENDC}"
 }
 
 cls() {
