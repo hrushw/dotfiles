0fbad6dc80632db288a03263a54b2c85bd410908
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2025-11-02 00:24:53 +0530

    bashrc cleanup

---
+++ .config/bash/colors.bash
@@ -0,0 +1,63 @@
+# Prompt color variables - must be loaded before prompt.bash
+BLACK="$(tput setaf 0)"
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+YELLOW="$(tput setaf 3)"
+BLUE="$(tput setaf 4)"
+MAGENTA="$(tput setaf 5)"
+CYAN="$(tput setaf 6)"
+WHITE="$(tput setaf 7)"
+
+ENDC="$(tput sgr0)"
+BOLD="$(tput bold)"
+
+if [ $(($(tput colors) > 16)) = "0" ]; then
+	B_BLACK="$BLACK"
+	B_RED="$RED"
+	B_GREEN="$GREEN"
+	B_YELLOW="$YELLOW"
+	B_BLUE="$BLUE"
+	B_MAGENTA="$MAGENTA"
+	B_CYAN="$CYAN"
+	B_WHITE="$WHITE"
+else
+	B_BLACK="$(tput setaf 8)"
+	B_RED="$(tput setaf 9)"
+	B_GREEN="$(tput setaf 10)"
+	B_YELLOW="$(tput setaf 11)"
+	B_BLUE="$(tput setaf 12)"
+	B_MAGENTA="$(tput setaf 13)"
+	B_CYAN="$(tput setaf 14)"
+	B_WHITE="$(tput setaf 15)"
+fi
+
+set_prompt() {
+	# use bright colors if supported by the terminal
+	PS0=""
+	local ps1uhostname="\[${B_RED}\][\[${B_GREEN}\]\u\[${B_WHITE}\]@\[${B_GREEN}\]\h\[${B_RED}\]]"
+	local ps1dir="\[${B_RED}\][ \[${B_CYAN}\]\w\[${B_RED}\] ]"
+	local ps1prompt="\[${B_RED}\][\[${B_WHITE}\]\$\[${B_RED}\]]> "
+	PS1="\[${ENDC}${BOLD}${B_WHITE}\]${ps1uhostname}${B_WHITE} : ${ps1dir}\n${ps1prompt}\[${ENDC}\]"
+	PS2="\[${ENDC}${BOLD}${B_RED}\]| \[${ENDC}\]"
+	# PS3 does not use \[\] escape sequences
+	PS3="${ENDC}${BOLD}$(tput setaf 9)[$(tput setaf 15)#$(tput setaf 9)]? $(tput sgr0)"
+}
+
+# Print terminal type and size
+tszfmtprint() {
+	set -- $(stty size)
+	local rows=$1
+	local cols=$2
+	local term_clr="${B_BLUE}${TERM}${B_RED}"
+	local cols_clr="${B_CYAN}${cols}${B_WHITE}"
+	local rows_clr="${B_CYAN}${rows}${B_RED}"
+	echo "${ENDC}${BOLD}${B_RED}[${term_clr}] ${B_WHITE}-${B_RED} [${cols_clr} x ${rows_clr}]${ENDC}"
+}
+
+# Colored output
+alias ls='ls --color=auto --group-directories-first'
+alias grep='grep --color=auto'
+alias diff='diff --color=auto'
+alias ip='ip --color=auto'
+
+
--- .config/bash/init.bash
+++ .config/bash/init.bash
@@ -1,13 +1,35 @@
-source $XDG_CONFIG_HOME/bash/vars.bash
+source $XDG_CONFIG_HOME/bash/colors.bash
 source $XDG_CONFIG_HOME/bash/functions.bash
-source $XDG_CONFIG_HOME/bash/aliases.bash
-source $XDG_CONFIG_HOME/bash/prompt.bash
 
-if [ "$TERM" = "eterm-color" ]; then
-	alias vim='echo "vim is not supported inside eterm"'
-else
-	set -o vi
-fi
+export HISTFILE=$XDG_STATE_HOME/bash/history
+export HISTSIZE=-1
+export LESS='-R --use-color -Dd+r$Du+b$ +0'
+export MANROFFOPT="-c"
+
+cls() {
+	tput reset
+	tabs -4
+	! [ "$NOHEADER" ] && tszfmtprint
+}
+
+lc () {
+	local termcols=$(set -- $(stty size); builtin echo $2)
+	[ $(("$1" < "$termcols")) != '0' ] && stty cols ${1}
+	ls --color=auto --group-directories-first ${@:2}
+	stty cols $termcols
+}
+alias l='lc 120'
+alias la='l -a'
+
+case "$TERM" in
+	"st-256color-kitty")
+		export KITTY_WINDOW_ID=t;;
+
+	"eterm-color")
+		alias vim='echo "vim is not supported inside eterm"';;
+	*)
+		set -o vi;;
+esac
 
 eval $(dircolors)
 if [ "$NOCLEAR" ]; then
@@ -15,5 +37,21 @@ if [ "$NOCLEAR" ]; then
 else
 	cls
 fi
+
+
+alias mv="mv -i"
+alias rm='echo "rm is disabled, use del"'
+alias del='mv -it ~/.local/share/trash/'
+
+alias FZF="fzf --style minimal --preview 'fzf-preview.sh {}' --bind 'focus:transform-header:file --brief {}'"
+
+
+# stops annoying font warning
+alias ngspice='LC_CTYPE=C ngspice'
+
+alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
+
+alias py='python'
+alias q="exit"
 
 set_prompt
