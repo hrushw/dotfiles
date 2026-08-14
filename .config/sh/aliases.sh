843c5fed164c5eb0fd4280e2c69bc6704deec485
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2026-08-15 04:50:01 +0530

    more consistent waste management

--- .config/sh/aliases.sh
+++ .config/sh/aliases.sh
@@ -5,7 +5,7 @@
 alias mv="mv -i"
 alias rm='echo "rm is disabled, use del"'
 alias pkill='pkill -x'
-alias del='mv -it ~/.local/share/trash/'
+alias del='mv -it $XDG_DATA_HOME/Trash'
 
 # stops annoying font warning
 alias ngspice='LC_CTYPE=C ngspice'
