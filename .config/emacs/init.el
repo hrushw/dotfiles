a497136e7d7429887f8978a0fd302778593f3e40
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2026-08-18 11:18:23 +0530

    .

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,3 +1,54 @@
+(use-package tool-bar
+  :custom
+  (tool-bar-mode nil))
+
+(use-package menu-bar
+  :custom
+  (menu-bar-mode nil))
+
+(use-package scroll-bar
+  :custom
+  (scroll-bar-mode nil))
+
+(use-package ido
+  :custom
+  (ido-enable-flex-matching t)
+  (ido-enable-regexp t)
+  (ido-mode 'both)
+  (ido-everywhere t))
+
+(use-package recentf
+  :custom
+  (recentf-save-file "~/.local/state/emacs/recentf"))
+
+(use-package bookmark
+  :custom
+  (bookmark-default-file "~/.config/emacs/bookmarks"))
+
+(use-package savehist
+  :custom
+  (savehist-file "~/.local/state/emacs/history")
+  (savehist-mode t))
+
+
+(setq backup-directory-alist '(("." . "~/.local/state/emacs/backups")))
+
+(setq make-backup-files t)
+(setq backup-by-copying t)
+(setq vc-follow-symlinks t)
+
+(setq default-directory "~")
+
+(load-theme 'modus-operandi-deuteranopia)
+
+(setq default-frame-alist
+      '((fullscreen . maximized)
+        (undecorated . t)
+        (font . "SpaceMono Nerd Font-16")
+        (vertical-scroll-bars . nil)
+        (horizontal-scroll-bars . nil)))
+
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name
@@ -18,18 +69,6 @@
 (straight-use-package 'use-package)
 (use-package straight)
 
-(use-package tool-bar
-  :custom
-  (tool-bar-mode nil))
-
-(use-package menu-bar
-  :custom
-  (menu-bar-mode nil))
-
-(use-package scroll-bar
-  :custom
-  (scroll-bar-mode nil))
-
 (use-package evil
   :straight t
   :custom
@@ -42,44 +81,10 @@
 
 (use-package evil-collection
   :straight t
+  :after evil
   :config
   (evil-collection-init))
 
-(use-package ido
-  :init
-  (setq ido-enable-flex-matching t
-	ido-enable-regexp t)
-  :config
-  (ido-mode 1)
-  (ido-everywhere 1))
-
-(use-package recentf
-  :init
-  (setq recentf-save-file "~/.local/state/emacs/recentf"))
-
-(use-package bookmark
-  :init
-  (setq bookmark-default-file "~/.config/emacs/bookmarks"))
-
-(use-package savehist
-  :init
-  (setq savehist-file "~/.local/state/emacs/history")
-  :config
-  (savehist-mode 1))
-
-(setq backup-directory-alist '(("." . "~/.local/state/emacs/backups")))
-
-(setq make-backup-files t)
-(setq backup-by-copying t)
-
-(setq default-directory "~")
-
-(load-theme 'modus-operandi-deuteranopia)
-
-(setq default-frame-alist
-      '((fullscreen . maximized)
-        (undecorated . t)
-        (font . "SpaceMono Nerd Font-16")
-        (vertical-scroll-bars . nil)
-        (horizontal-scroll-bars . nil)))
+(use-package magit
+  :straight t)
 
